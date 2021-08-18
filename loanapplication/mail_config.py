e70321e31e565aed0b9440f609fab5ed8511f868
LokRaj Kumar Vuppu <63893671+LokRaj-Vuppu@users.noreply.github.com>
2021-08-18 08:49:07 +0530

    Update mail_config.py

--- loanapplication/mail_config.py
+++ loanapplication/mail_config.py
@@ -2,8 +2,8 @@ MAIL_SERVER = 'smtp.googlemail.com'
 MAIL_PORT = 587
 MAIL_USE_TLS = True
 MAIL_USE_SSL=False
-MAIL_USERNAME = 'lokrajkumarvuppu@gmail.com'
-MAIL_PASSWORD = 'Raj@12345'
-MAIL_DEFAULT_SENDER=('LokRaj Kumar','lokrajkumarvuppu@gmail.com')
+MAIL_USERNAME = 'sender_email'
+MAIL_PASSWORD = 'password'
+MAIL_DEFAULT_SENDER=('sender_name','sender_email')
 
 
